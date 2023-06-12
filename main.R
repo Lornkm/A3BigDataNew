43ad80776bfa62ffedf8ceb6ffd46b10a1692545
Clément Branger <clement.branger@isen-ouest.yncrea.fr>
2023-06-12 14:28:17 +0200

    Suppression valeurs incohérentes ou manquantes

--- main.R
+++ main.R
@@ -1,13 +1,6 @@
 
-accidents <- read.csv("stat_acc_V3.csv", sep = "\t", header = TRUE)
+accidents <- read.csv("stat_acc_V3.csv",sep = ";", header = TRUE)
 
+na_rows <- complete.cases(accidents)
 
-
-# Créer un vecteur de nombres
-x <- c(1, 2, 3, 4, 5)
-print(x)
-# Calculer la moyenne du vecteur
-mean(x)
-
-print("lrkn << ndtr")
-
+mydata_clean <- mydata[na_rows, ]
