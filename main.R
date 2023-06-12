99fb398c7e5f61e5fa5eaeb1724b6057df6ce96d
Clément Branger <clement.branger@isen-ouest.yncrea.fr>
2023-06-12 14:37:38 +0200

    Suppression valeurs incohérentes ou manquantes

--- main.R
+++ main.R
@@ -1,6 +1,11 @@
 
 accidents <- read.csv("stat_acc_V3.csv",sep = ";", header = TRUE)
 
+# Remplacer les valeurs NULL par NA
+accidents[is.null(accidents)] <- NA
+
+# Identifier les observations contenant des valeurs manquantes
 na_rows <- complete.cases(accidents)
 
-mydata_clean <- mydata[na_rows, ]
+# Supprimer les observations contenant des valeurs manquantes
+accidents_clean <- accidents[na_rows, ]
