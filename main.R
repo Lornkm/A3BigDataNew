fd03ca64b5d4a3452565b1bb6ba93bc0dcef91ef
Clément Branger <clement.branger@isen-ouest.yncrea.fr>
2023-06-12 16:14:38 +0200

    suppression des cases vides dans accident directement

--- main.R
+++ main.R
@@ -18,8 +18,7 @@ num_cols <- sapply(accidents, is.numeric)
 col_means <- colMeans(accidents[, num_cols], na.rm = TRUE)
 
 # Imputer les valeurs manquantes par la moyenne de chaque colonne numérique
-accidents_imputés <- accidents
 for (i in seq_along(col_means)) {
   col_name <- names(col_means)[i]
-    accidents_imputés[is.na(accidents_imputés[, col_name]), col_name] <- col_means[i]
+    accidents[is.na(accidents[, col_name]), col_name] <- col_means[i]
 }
