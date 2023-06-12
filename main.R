8a5808e3a03896f5c46e1dda2b23953c90142508
Lornkm <loriane.kim@isen-ouest.yncrea.fr>
2023-06-12 15:52:31 +0200

    traitement données char en int

--- main.R
+++ main.R
@@ -1,33 +1,29 @@
 accidents <- read.csv('stat_acc_V3.csv', sep = ";", header = TRUE)
 
+#transforme les données char en int
 
 #traitements données id_code_insee
-id_code_insee = accidents$id_code_insee
-#transforme les données char en int
-id_code_insee = as.integer(id_code_insee)
+accidents$id_code_insee = as.integer(accidents$id_code_insee)
 #remplace les NA par la moyenne
-id_code_insee[is.na(id_code_insee)] <- as.integer(mean(id_code_insee, na.rm = TRUE))
+accidents$id_code_insee[is.na(accidents$id_code_insee)] <- as.integer(mean(accidents$id_code_insee, na.rm = TRUE))
 #transforme les données float en int
-id_code_insee = as.integer(id_code_insee)
-print(id_code_insee)
-print(class(id_code_insee))
+accidents$id_code_insee = as.integer(accidents$id_code_insee)
+print(accidents$id_code_insee)
 
 
 #traitements données age
 #même méthode pour l'age
-age = accidents$age
-age = as.integer(age)
-age[is.na(age)] <- as.integer(mean(age, na.rm = TRUE))
-age = as.integer(age)
+accidents$age = as.integer(accidents$age)
+accidents$age[is.na(accidents$age)] <- as.integer(mean(accidents$age, na.rm = TRUE))
+accidents$age = as.integer(accidents$age)
 print(age)
 
 #traitements données an_nais
 #même méthode pour l'année
-annee_naissance = accidents$an_nais
-annee_naissance = as.integer(annee_naissance)
-annee_naissance[is.na(annee_naissance)] <- as.integer(mean(annee_naissance, na.rm = TRUE))
-annee_naissance = as.integer(annee_naissance)
-print(annee_naissance)
+accidents$an_nais = as.integer(accidents$an_nais)
+accidents$an_nais[is.na(annee_naissance)] <- as.integer(mean(accidents$an_nais, na.rm = TRUE))
+accidents$an_nais = as.integer(accidents$an_nais)
+print(accidents$an_nais)
 
 
 
