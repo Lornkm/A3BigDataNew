9d0eb0943685c518d6ad2deac54a19949c6edb04
Clément Branger <clement.branger@isen-ouest.yncrea.fr>
2023-06-12 16:01:06 +0200

    Suppression valeurs incohérentes ou manquantes

--- main.R
+++ main.R
@@ -1,38 +1,8 @@
+
 accidents <- read.csv('stat_acc_V3.csv', sep = ";", header = TRUE)
-
-#transforme les données char en int
-
-#traitements données id_code_insee
-accidents$id_code_insee = as.integer(accidents$id_code_insee)
-#remplace les NA par la moyenne
-accidents$id_code_insee[is.na(accidents$id_code_insee)] <- as.integer(mean(accidents$id_code_insee, na.rm = TRUE))
-#transforme les données float en int
-accidents$id_code_insee = as.integer(accidents$id_code_insee)
-print(accidents$id_code_insee)
-
-
-#traitements données age
-accidents$age = as.integer(accidents$age)
-accidents$age[is.na(accidents$age)] <- as.integer(mean(accidents$age, na.rm = TRUE))
-accidents$age = as.integer(accidents$age)
-print(age)
-
-#traitements données an_nais
-accidents$an_nais = as.integer(accidents$an_nais)
-accidents$an_nais[is.na(accidents$an_nais)] <- as.integer(mean(accidents$an_nais, na.rm = TRUE))
-accidents$an_nais = as.integer(accidents$an_nais)
-print(accidents$an_nais)
-
-#traitements données place
-accidents$place = as.integer(accidents$place)
-accidents$place[is.na(accidents$place)] <- as.integer(mean(accidents$place, na.rm = TRUE))
-accidents$place = as.integer(accidents$place)
-print(accidents$place)
-
-
-
 accidents$descr_cat_veh <- as.numeric(factor(accidents$descr_cat_veh))
 print(accidents$descr_cat_veh[1:20])
+
 
 
 # Remplacer les valeurs NULL par NA
@@ -48,7 +18,5 @@ col_means <- colMeans(accidents[, num_cols], na.rm = TRUE)
 accidents_imputés <- accidents
 for (i in seq_along(col_means)) {
   col_name <- names(col_means)[i]
-  accidents_imputés[is.na(accidents_imputés[, col_name]), col_name] <- col_means[i]
+    accidents_imputés[is.na(accidents_imputés[, col_name]), col_name] <- col_means[i]
 }
-
-print(accidents$place)
