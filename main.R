b68dfb02e153b6bd64ba22d1a7e5ccd0bf4ae71e
Clément Branger <clement.branger@isen-ouest.yncrea.fr>
2023-06-12 14:59:12 +0200

    Suppression valeurs incohérentes ou manquantes

--- main.R
+++ main.R
@@ -1,5 +1,5 @@
 
-accidents <- read.csv('/Users/noedetre/A3BigDataNew/A3BigDataNew/stat_acc_V3.csv', sep = ";", header = TRUE)
+accidents <- read.csv('stat_acc_V3.csv', sep = ";", header = TRUE)
 accidents$descr_cat_veh <- as.numeric(factor(accidents$descr_cat_veh))
 print(accidents$descr_cat_veh[1:20])
 
@@ -8,7 +8,15 @@ print(accidents$descr_cat_veh[1:20])
 # Remplacer les valeurs NULL par NA
 accidents[is.null(accidents)] <- NA
 
-# Identifier les observations contenant des valeurs manquantes
-na_rows <- complete.cases(accidents)
+# Identifier les colonnes numériques
+num_cols <- sapply(accidents, is.numeric)
 
-print("lrkn << ndtr")
+# Calculer la moyenne de chaque colonne numérique en ignorant les valeurs manquantes
+col_means <- colMeans(accidents[, num_cols], na.rm = TRUE)
+
+# Imputer les valeurs manquantes par la moyenne de chaque colonne numérique
+accidents_imputés <- accidents
+for (i in seq_along(col_means)) {
+  col_name <- names(col_means)[i]
+  accidents_imputés[is.na(accidents_imputés[, col_name]), col_name] <- col_means[i]
+}
