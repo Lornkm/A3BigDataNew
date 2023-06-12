8d17faa0bc079b8238bf77c1873323ac943f499d
Lornkm <loriane.kim@isen-ouest.yncrea.fr>
2023-06-12 15:46:03 +0200

    traitement données char en int

--- main.R
+++ main.R
@@ -1,12 +1,32 @@
+accidents = read.csv('stat_acc_V3.csv', sep=";")
 
-accidents = read.csv('C:\Users\loria\Documents\Cours\Big Data\A3BigDataNew\stat_acc_V3.csv')
+print(accidents$ville)
+
+#traitements données id_code_insee
+id_code_insee = accidents$id_code_insee
+#transforme les données char en int
+id_code_insee = as.integer(id_code_insee)
+#remplace les NA par la moyenne
+id_code_insee[is.na(id_code_insee)] <- as.integer(mean(id_code_insee, na.rm = TRUE))
+#transforme les données float en int
+id_code_insee = as.integer(id_code_insee)
+print(id_code_insee)
+print(class(id_code_insee))
 
 
-# Créer un vecteur de nombres
-x <- c(1, 2, 3, 4, 5)
-print(x)
-# Calculer la moyenne du vecteur
-mean(x)
+#traitements données age
+#même méthode pour l'age
+age = accidents$age
+age = as.integer(age)
+age[is.na(age)] <- as.integer(mean(age, na.rm = TRUE))
+age = as.integer(age)
+print(age)
 
-print("lrkn << ndtr")
+#traitements données an_nais
+#même méthode pour l'année
+annee_naissance = accidents$an_nais
+annee_naissance = as.integer(annee_naissance)
+annee_naissance[is.na(annee_naissance)] <- as.integer(mean(annee_naissance, na.rm = TRUE))
+annee_naissance = as.integer(annee_naissance)
+print(annee_naissance)
 
