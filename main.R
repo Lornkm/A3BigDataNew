a283b104ab76ba02ffcb91086f995c4391767eb6
Clément Branger <clement.branger@isen-ouest.yncrea.fr>
2023-06-13 14:14:07 +0200

    Ajout des représentations graphiques

--- main.R
+++ main.R
@@ -1,11 +1,50 @@
-
 accidents <- read.csv('stat_acc_V3.csv', sep = ";", header = TRUE)
-accidents$descr_cat_veh <- as.numeric(factor(accidents$descr_cat_veh))
-accidents$ville <- as.numeric(factor(accidents$ville))
-print(accidents$descr_cat_veh[1:20])
+
+#transforme les données char en int
+
+#traitements données id_code_insee
+accidents$id_code_insee = as.integer(accidents$id_code_insee)
+#remplace les NA par la moyenne
+accidents$id_code_insee[is.na(accidents$id_code_insee)] <- as.integer(mean(accidents$id_code_insee, na.rm = TRUE))
+#transforme les données float en int
+accidents$id_code_insee = as.integer(accidents$id_code_insee)
+#print(accidents$id_code_insee)
 
 
+#traitements données age
+accidents$age = as.integer(accidents$age)
+accidents$age[is.na(accidents$age)] <- as.integer(mean(accidents$age, na.rm = TRUE))
+accidents$age = as.integer(accidents$age)
+#print(age)
 
+#traitements données an_nais
+accidents$an_nais = as.integer(accidents$an_nais)
+accidents$an_nais[is.na(accidents$an_nais)] <- as.integer(mean(accidents$an_nais, na.rm = TRUE))
+accidents$an_nais = as.integer(accidents$an_nais)
+#print(accidents$an_nais)
+
+#traitements données place
+accidents$place = as.integer(accidents$place)
+accidents$place[is.na(accidents$place)] <- as.integer(mean(accidents$place, na.rm = TRUE))
+accidents$place = as.integer(accidents$place)
+#print(accidents$an_nais)
+
+
+#variables multimodales en nombres
+print(unique(accidents$descr_grav))
+# Obtenir les niveaux uniques de la variable
+levels <- unique(accidents$descr_grav)  # Obtient les niveaux uniques de la variable
+for (i in 1:length(levels)) {
+  accidents$descr_grav[accidents$descr_grav == levels[i]] <- i
+}
+print(unique(accidents$descr_grav))
+# Obtenir les niveaux uniques de la variable
+levels <- unique(accidents$descr_cat_veh)
+print(unique(accidents$descr_cat_veh))
+for (i in 1:length(levels)) {
+  accidents$descr_cat_veh[accidents$descr_cat_veh == levels[i]] <- i
+}
+print(unique(accidents$descr_cat_veh))
 
 
 # Remplacer les valeurs NULL par NA
@@ -22,3 +61,71 @@ for (i in seq_along(col_means)) {
   col_name <- names(col_means)[i]
     accidents[is.na(accidents[, col_name]), col_name] <- col_means[i]
 }
+
+#print(accidents$place)
+
+
+# Mettre le bon âge
+print(accidents$age[1:20])
+accidents$age <- accidents$age - 14
+print(accidents$age[1:20])
+
+
+# Calculer le nombre d'accidents par conditions atmosphériques
+atmospheric_accidents <- aggregate(list(count = accidents$Num_Acc), by = list(atmospheric = accidents$descr_athmo), FUN = length)
+
+# Créer un graphique à barres pour les accidents par conditions atmosphériques
+barplot(atmospheric_accidents$count, names.arg = atmospheric_accidents$atmospheric,
+        xlab = "Conditions atmosphériques", ylab = "Nombre d'accidents",
+        main = "Nombre d'accidents en fonction des conditions atmosphériques")
+
+
+
+# Calculer le nombre d'accidents par description de la surface
+surface_accidents <- aggregate(list(count = accidents$Num_Acc), by = list(surface = accidents$descr_etat_surf), FUN = length)
+
+# Créer un graphique à barres pour les accidents par description de la surface
+barplot(surface_accidents$count, names.arg = surface_accidents$surface,
+        xlab = "Description de la surface", ylab = "Nombre d'accidents",
+        main = "Nombre d'accidents en fonction de la description de la surface")
+
+
+
+# Calculer le nombre d'accidents par gravité
+severity_accidents <- aggregate(list(count = accidents$Num_Acc), by = list(severity = accidents$descr_grav), FUN = length)
+
+# Créer un graphique à barres pour les accidents par gravité
+barplot(severity_accidents$count, names.arg = severity_accidents$severity,
+        xlab = "Gravité", ylab = "Nombre d'accidents",
+        main = "Nombre d'accidents selon la gravité")
+
+
+
+
+
+# Extraire l'heure de la colonne date
+accidents$hour <- as.numeric(format(accidents$date, "%H"))+1
+
+# Calculer le nombre d'accidents par heure
+hourly_accidents <- aggregate(list(count = accidents$Num_Acc), by = list(hour = accidents$hour), FUN = length)
+
+# Créer un graphique à barres pour les accidents par heure
+barplot(hourly_accidents$count, names.arg = hourly_accidents$hour,
+        xlab = "Heure", ylab = "Nombre d'accidents",
+        main = "Nombre d'accidents par tranches d'heure")
+
+
+
+# Calculate the number of accidents by city
+city_accidents <- aggregate(list(count = accidents$Num_Acc), by = list(city = accidents$ville), FUN = length)
+
+# Order the data by the number of accidents in descending order
+city_accidents <- city_accidents[order(-city_accidents$count),]
+
+# Select the top 20 cities with the most accidents
+top_100_cities <- head(city_accidents, 100)
+
+# Create a barplot of the number of accidents by city
+barplot(top_100_cities$count, names.arg = top_100_cities$city,
+        xlab = "Code postal", ylab = "Nombre d'accidents",
+        main = "Nombre d'accidents par ville", las = 2, cex.names = 0.8)
