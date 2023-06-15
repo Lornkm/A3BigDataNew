a6ed67c4725701ecbf1d46e74b6597219f6c52fd
Noe Detre <noe.detre@isen-ouest.yncrea.fr>
2023-06-15 08:22:21 +0200

    "tableau croisée et chi_2"

--- analyse_de_donnees.r
+++ analyse_de_donnees.r
@@ -12,29 +12,29 @@ accidents$age_group <- cut(accidents$age, breaks = c(0, 17, 24, 34, 44, 54, 64, 
 # Afficher les premières lignes pour vérification
 print(accidents$age_group[1:20])
 
-# Test du chi2 sur descr_grav
-tableau_croise <- table(accidents$descr_grav, accidents$age_group)
-print(tableau_croise)
-result_chi2 <- chisq.test(tableau_croise)
+# Test du chi2 sur descr_grav  et âge
+tableau_croise_age_grav <- table(accidents$descr_grav, accidents$age_group)
+print(tableau_croise_age_grav)
+result_chi2_age_grav <- chisq.test(tableau_croise_age_grav)
 # Afficher les résultats du test
-print(result_chi2)
+print(result_chi2_age_grav)
 
-mosaicplot(tableau_croise)
+mosaicplot(tableau_croise_age_grav)
 
 
 #tableau croisé entre gravité et dispositif de sécurité
 tableau_croise_secu_grav <- table(accidents$descr_grav, accidents$descr_dispo_secu)
 print(tableau_croise_secu_grav)
-result_chi2_2 <- chisq.test(tableau_croise_secu_grav)
-print(result_chi2_2)
+result_chi2_secu_grav <- chisq.test(tableau_croise_secu_grav)
+print(result_chi2_secu_grav)
 mosaicplot(tableau_croise_secu_grav, las=1, main = "Mosaïque entre la gravité et le dispositif de sécurité")
 
 
 #tableau croisé entre gravité et la description de la catégorie du véhicule
 tableau_croise_catveh_grav <- table(accidents$descr_grav, accidents$descr_cat_veh)
 print(tableau_croise_catveh_grav)
-result_chi2_3 <- chisq.test(tableau_croise_catveh_grav)
-print(result_chi2_3)
+result_chi2_catveh_grav <- chisq.test(tableau_croise_catveh_grav)
+print(result_chi2_catveh_grav)
 mosaicplot(tableau_croise_catveh_grav, las = 1, main = "Mosaïque entre la gravité et la catégorie du véhicule")
 
 
